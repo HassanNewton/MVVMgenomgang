a7cf5b10667b60fa05bedbc47e7c82a3daacdb24
fleurkeks <lea.togotzes@googlemail.com>
2024-10-16 10:46:54 +0200

    RelayCommand, AddCommnad, deleteCommand

--- MVVMgenomgang/ViewModel/MainWindowViewModel.cs
+++ MVVMgenomgang/ViewModel/MainWindowViewModel.cs
@@ -8,7 +8,63 @@ namespace MVVMgenomgang.ViewModel
     {
         public ObservableCollection<Item> Items { get; set; }
 
+        //Alternativ 1
+        //public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+        //public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => selectedItem != null);
 
+
+        //Alternativ 2
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                if (addCommand == null)
+                {
+                    addCommand = new RelayCommand(ExecuteAddItem);
+                }
+                return addCommand;
+            }
+
+        }
+
+        private void ExecuteAddItem(object parameter)
+        {
+            AddItem();
+        }
+        private void AddItem()
+        {
+            Items.Add(new Item { Name = "New Item", SerieNbr = "XXXX", Quantity = 2 });
+        }
+
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                if (deleteCommand == null)
+                {
+                    deleteCommand = new RelayCommand(ExecuteDeleteItem, CanExecuteDeleteItem);
+                }
+                return deleteCommand;
+            }
+
+        }
+        private bool CanExecuteDeleteItem(object parameter)
+        {
+            return selectedItem != null;
+        }
+
+        private void ExecuteDeleteItem(object parameter)
+        {
+            DeleteItem();
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(selectedItem);
+        }
 
 
         public MainWindowViewModel()
